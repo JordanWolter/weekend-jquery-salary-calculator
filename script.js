35c0b8ae5e0604032f9a4e9971d169bc0ba7a937
Jordan Wolter <jordan.wolter@icloud.com>
2022-09-24 13:04:36 -0500

    created missing input function and clear input function

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let workForce = [];
+let employeeArray = [];
 let i = 0;
 
 $(document).ready(readyNow);
@@ -7,11 +7,23 @@ function readyNow(){
     console.log('DOM, for the family!');
 
     $('#salaryCalculator').on('submit', addEmployee);
+
+    $('#employeeTable').on('click', '.delete', dltEmployee);
 }
 
 function addEmployee(event){
 
     event.preventDefault();
+
+    // if($('#firstName').val() === '' || $('#lastName').val() === '' || $('#id').val() === '' || $('#position').val() === '' || $('#salary').val() === ''){
+    //     alert('!!!MISSING INPUT FIELD!!!');
+    //     return;
+    // }
+
+    if(missingInput()){
+        clearInput();
+        return;
+    }
 
     console.log('Adding new member to the family', $(this));
 
@@ -25,39 +37,82 @@ function addEmployee(event){
     }
 
     console.log('New family member is', newEmployee);
-    workForce.push(newEmployee);
-    console.log('Members of the family', workForce);
+    employeeArray.push(newEmployee);
+    console.log('Members of the family', employeeArray);
 
-    console.log(newEmployee.employeeIndex);
     i++
     console.log(newEmployee.employeeIndex);
 
-    render()
+    render();
+
 
 }
 
 function render(){
-    $('#employeeTable > :not(.rowTitle) a').empty();
+    //$('#employeeTable > tr > :not(.rowTitle)').empty();
+    //$('#employeeTable').empty();
+    $('#employeeTable').find('tr:gt(0)').remove();
 
-    for(let employee of workForce){
+    //<td id='delete'>${employeeArray.indexOf(employee)}</td>
+
+    for(let employee of employeeArray){
         $('#employeeTable').append(`
         <tr>
         <td>${employee.firstName}</td>
         <td>${employee.lastName}</td>
         <td>${employee.idNum}</td>
         <td>${employee.jobTitle}</td>
-        <td>${employee.salary}</td>
+        <td>$${employee.salary}</td>
         <td>
         <button class="delete">Delete</button>
         </td>
         </tr>
         `)
     }
+    clearInput();
+
 }
 
-function dltEmployee(){
+function dltEmployee(employee){
     console.log('In dltEmployee $(this)', $(this));
 
-    $(this).partent().partent().remove();
+    $(this).parent().parent().remove();
 
+    //let deleteThis = $(this).parent().parent().find('#delete').html();
+
+    let deleteThis = $(this).parent().parent().find(employee.employeeIndex);
+    console.log(deleteThis);
+
+    employeeArray.splice(deleteThis, 1);
+
+}
+
+function clearInput(){
+    $('#firstName').val('');
+    $('#lastName').val('');
+    $('#id').val('');
+    $('#position').val('');
+    $('#salary').val('');
+}
+
+function missingInput(){
+    if($('#firstName').val() === ''){
+        alert('!!!MISSING FIRST NAME FIELD!!!');
+        return true;
+    }else if($('#lastName').val() === ''){
+        alert('!!!MISSING LAST NAME FIELD!!!');
+        return true;
+    }else if($('#id').val() === ''){
+        alert('!!!MISSING ID FIELD!!!');
+        return true;
+    }else if($('#position').val() === '' ){
+        alert('!!!MISSING TITLE FIELD!!!');
+        return true;
+    }else if($('#salary').val() === ''){
+        alert('!!!MISSING SALARY FIELD!!!');
+        return true;
+    }else{
+        console.log('No missing inputs');
+        return false;
+    }
 }
