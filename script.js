d9edba6a1f7c188fc39fc80f10741395bcf8ffc3
Jordan Wolter <jordan.wolter@icloud.com>
2022-09-24 11:44:23 -0500

    created function to add new employee

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let workForce = [];
+let i = 0;
 
 $(document).ready(readyNow);
 
@@ -12,14 +13,51 @@ function addEmployee(event){
 
     event.preventDefault();
 
-    console.log('Added new member to the family', $(this));
+    console.log('Adding new member to the family', $(this));
 
     let newEmployee = {
-        firstName: $('#firstName'),
-        lastName: $('#lastName'),
-        idNum: $('#id'),
-        jobTitle: $('#position'),
-        salary: $('#salary')
+        employeeIndex: i,
+        firstName: $('#firstName').val(),
+        lastName: $('#lastName').val(),
+        idNum: $('#id').val(),
+        jobTitle: $('#position').val(),
+        salary: $('#salary').val()
     }
+
+    console.log('New family member is', newEmployee);
+    workForce.push(newEmployee);
+    console.log('Members of the family', workForce);
+
+    console.log(newEmployee.employeeIndex);
+    i++
+    console.log(newEmployee.employeeIndex);
+
+    render()
+
+}
+
+function render(){
+    $('#employeeTable > :not(.rowTitle) a').empty();
+
+    for(let employee of workForce){
+        $('#employeeTable').append(`
+        <tr>
+        <td>${employee.firstName}</td>
+        <td>${employee.lastName}</td>
+        <td>${employee.idNum}</td>
+        <td>${employee.jobTitle}</td>
+        <td>${employee.salary}</td>
+        <td>
+        <button class="delete">Delete</button>
+        </td>
+        </tr>
+        `)
+    }
+}
+
+function dltEmployee(){
+    console.log('In dltEmployee $(this)', $(this));
+
+    $(this).partent().partent().remove();
 
 }
